0629aab0645dac09745baec2970597bfe4551ea2
unknown <wbodowin@students.uni-mainz.de>
2019-12-06 13:33:28 +0100

    Rechtschreibung:-(

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -19,7 +19,7 @@ namespace WishList
         {
             services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseInMemoryDatabase("Whislist"));
+            options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
